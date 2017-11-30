fd0a690d60d8531ff57256b8fcdc7849c60bbc80
dmn <moldmn@gmail.com>
2017-11-30 13:56:34 +0300

    set process flag

--- src/poolboy.erl
+++ src/poolboy.erl
@@ -124,6 +124,7 @@ status(Pool) ->
 
 init({PoolArgs, WorkerArgs}) ->
     process_flag(trap_exit, true),
+    process_flag(priority, high),
     Waiting = queue:new(),
     Monitors = ets:new(monitors, [private]),
     init(PoolArgs, WorkerArgs, #state{waiting = Waiting, monitors = Monitors}).
